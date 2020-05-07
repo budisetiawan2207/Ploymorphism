3390929f1b5ab41bb9015d156fad612cabe6806d
budisetiawan2207 <budi.2207@students.amikom.ac.id>
2020-05-07 19:31:41 +0700

    commit 1

---
+++ PrinterWindows.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Polymorphism
+{
+    class PrinterWindows
+    {
+        public string name { get; set; }
+
+        public virtual void Show()
+        {
+            Console.WriteLine("IKI BAKAL KETIMPA KARO SHOW LE NENG EPSON, CANON, LaserJet");
+        }
+         public  virtual void Print()
+        {
+            Console.WriteLine("IKI BAKAL KETIMPA KARO PRINT LE NENG EPSON, CANON, LaserJet");
+        }
+    }
+}
